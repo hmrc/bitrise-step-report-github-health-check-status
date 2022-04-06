843227a24f8d64aa8851a8a2396d0d3f3dde30b0
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-04-06 10:29:08 +0100

    Fixing typo

--- step.sh
+++ step.sh
@@ -11,7 +11,7 @@ fi
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -H 'Accept: application/vnd.github.v3+json' \
     -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$BITRISEIO_GIT_REPOSITORY_SLUG/statuses/$BITRISE_GIT_COMMIT \
-    -d '{"state":"'"$STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
+    -d '{"state":"'"$STEP_STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
 )
 
 if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
