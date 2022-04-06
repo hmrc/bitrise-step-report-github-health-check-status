bad54d79fdaf62a26bf76913812b6cee0c3ea03b
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-04-06 10:16:45 +0100

    chore/error-handling - handling non 200 responses from CURL

--- step.sh
+++ step.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 # fail if any commands fails
 set -e
-# debug log
-set -x
 
-STATUS="failure"
+STEP_STATUS="failure"
 
 if [ "$BITRISE_BUILD_STATUS" == 0 ]; then
-    STATUS="success"
+    STEP_STATUS="success"
 fi
 
-curl \
--X POST -H 'Accept: application/vnd.github.v3+json' \
--H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$REPO_NAME/statuses/$BITRISE_GIT_COMMIT \
--d '{"state":"'"$STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
+    -X POST -H 'Accept: application/vnd.github.v3+json' \
+    -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$BITRISEIO_GIT_REPOSITORY_SLUG/statuses/$BITRISE_GIT_COMMIT \
+    -d '{"state":"'"$STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
+)
 
-if [ "$STATUS" == "failure" ]; then
+if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
+    if [ "$STEP_STATUS" == "failure" ]; then
+        exit 1
+    fi
+else
+    echo "curl failed"
     exit 1
 fi
+
