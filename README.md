6ed23fd8dadb1d71c5edf2fc0d843c494c1238ff
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2022-04-07 08:47:28 +0100

    Minor tweaks

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Updates the Github health status for a given pull request
 
 ## How to use this Step
 
+Add the following to your `bitrise.yml`
 ```
 - git::https://github.com/hmrc/bitrise-step-report-github-health-check-status.git@main:
     title: Report Github Health Check Status
+    is_always_run: true
 ```
--- step.sh
+++ step.sh
@@ -2,16 +2,18 @@
 # fail if any commands fails
 set -e
 
+STATUS_DESCRIPTION="Failed"
 STEP_STATUS="failure"
 
 if [ "$BITRISE_BUILD_STATUS" == 0 ]; then
     STEP_STATUS="success"
+    STATUS_DESCRIPTION="Passed"
 fi
 
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -H 'Accept: application/vnd.github.v3+json' \
     -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$BITRISEIO_GIT_REPOSITORY_SLUG/statuses/$BITRISE_GIT_COMMIT \
-    -d '{"state":"'"$STEP_STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
+    -d '{"state":"'"$STEP_STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$STATUS_DESCRIPTION"' - '"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
 )
 
 if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
